f3bb93ba183f67b475c1d7868475582bb1d9f993
none23 <n.anisimov.23@gmail.com>
2019-04-13 21:58:59 +0300

    v6.1.0

--- index.js
+++ index.js
@@ -1,111 +1,121 @@
+const path = require('path');
+
 const config = {
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
+    ecmaVersion: 2019,
+    sourceType: 'module',
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
       generators: true,
-      jsx: true
-    }
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
   },
 
   extends: [
-    "airbnb",
-    "plugin:eslint-comments/recommended",
-    "plugin:redux-saga/recommended",
-    "plugin:prettier/recommended",
-    "plugin:flowtype/recommended"
+    'airbnb',
+    'prettier',
+    'plugin:eslint-comments/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:react/recommended',
+    'prettier/react',
+    'prettier/unicorn',
+    'prettier/flowtype',
+    'plugin:flowtype/recommended',
   ],
-
   settings: {
-    "import/ignore": ["node_modules"],
-    "import/extensions": [".js", ".json"],
-    "import/resolver": { node: { paths: path.resolve(__dirname, ".") } }
+    'import/ignore': ['node_modules'],
+    'import/extensions': ['.js', '.json'],
   },
 
   plugins: [
-    "eslint-comments",
-    "flowtype",
-    "import",
-    "jest",
-    "jsx-a11y",
-    "node",
-    "prettier",
-    "react",
-    "redux-saga"
+    'eslint-comments',
+    'flowtype',
+    'import',
+    'graphql',
+    'jsx-a11y',
+    'react',
+    'sonarjs',
+    'unicorn',
   ],
 
   rules: {
-    camelcase: "off",
-    "no-nested-ternary": "off",
-    "no-multiple-empty-lines": ["error", { max: 3, maxEOF: 0 }],
-    "no-underscore-dangle": "off",
-    "no-unreachable": "error",
-    "no-unused-expressions": [
-      "error",
+    'no-console': 'err',
+    'no-debugger': 'err',
+    'no-multiple-empty-lines': ['error', { max: 3, maxEOF: 0 }],
+    'no-underscore-dangle': 'off',
+    'no-unused-expressions': [
+      'error',
       {
         allowShortCircuit: true,
         allowTernary: true,
-        allowTaggedTemplates: true
-      }
+        allowTaggedTemplates: true,
+      },
     ],
-    "no-useless-computed-key": "error",
-    "no-useless-constructor": "error",
-    "no-useless-rename": [
-      "error",
-      { ignoreDestructuring: false, ignoreImport: false, ignoreExport: false }
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'error',
+    'no-useless-rename': [
+      'error',
+      { ignoreDestructuring: false, ignoreImport: false, ignoreExport: false },
     ],
-    "object-curly-newline": "off",
-    "padded-blocks": [
-      "error",
-      { blocks: "never", switches: "never", classes: "never" }
+    'object-curly-newline': 'off',
+    'padded-blocks': [
+      'error',
+      { blocks: 'never', switches: 'never', classes: 'never' },
     ],
-    "prefer-const": "error",
-    radix: "off",
+    'prefer-const': 'error',
 
-    "eslint-comments/no-unused-disable": "error",
-    "eslint-comments/no-unused-enable": "error",
+    'eslint-comments/no-unused-disable': 'error',
+    'eslint-comments/no-unused-enable': 'error',
 
-    "import/default": "error",
-    "import/named": "error",
-    "import/namespace": "error",
-    "import/extensions": ["error", "never"],
-    "import/no-named-as-default": "off",
-    "import/prefer-default-export": "off",
+    'import/default': 'error',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/extensions': ['error', 'never'],
 
-    "jsx-a11y/aria-props": "error",
-    "jsx-a11y/aria-role": "error",
-    "jsx-a11y/heading-has-content": "error",
-    "jsx-a11y/href-no-hash": "off",
-    "jsx-a11y/label-has-for": "off",
-    "jsx-a11y/mouse-events-have-key-events": "error",
-    "jsx-a11y/role-has-required-aria-props": "error",
-    "jsx-a11y/role-supports-aria-props": "error",
-    "jsx-a11y/img-redundant-alt": "error",
-    "jsx-a11y/no-access-key": "error",
+    'jsx-a11y/aria-props': 'error',
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/heading-has-content': 'error',
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/mouse-events-have-key-events': 'error',
+    'jsx-a11y/role-has-required-aria-props': 'error',
+    'jsx-a11y/role-supports-aria-props': 'error',
+    'jsx-a11y/img-redundant-alt': 'error',
+    'jsx-a11y/no-access-key': 'error',
+    'jsx-a11y/anchor-is-valid': 'off',
 
-    "node/no-deprecated-api": "error",
-    "node/process-exit-as-throw": "error",
+    // 'node/no-deprecated-api': 'error',
+    // 'node/process-exit-as-throw': 'error',
 
-    "react/forbid-prop-types": ["error", { forbid: ["any"] }],
-    "react/default-props-match-prop-types": "off", // Flow
-    "react/jsx-curly-brace-presence": "off", // styled-jsx
-    "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
-    "react/no-array-index-key": "off",
-    "react/no-unused-prop-types": "off", // Flow
-    "react/prefer-stateless-function": [
-      "error",
-      { ignorePureComponents: true }
-    ],
-    "react/sort-comp": "off",
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': ['error', { forbid: ['any'] }],
+    'react/default-props-match-prop-types': 'off', // Flow
+    'react/jsx-curly-brace-presence': 'off', // styled-jsx
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+    'react/no-array-index-key': 'off',
+    'react/no-unused-prop-types': 'off', // Flow
+    'react/prefer-stateless-function': 'off',
 
-    "flowtype/define-flow-type": "error",
-    "flowtype/require-valid-file-annotation": "error",
-    "flowtype/use-flow-type": "error",
+    'flowtype/define-flow-type': 'error',
+    'flowtype/require-valid-file-annotation': 'error',
+    'flowtype/use-flow-type': 'error',
+    'flowtype/array-style-complex-type': ['error', 'verbose'],
+    'flowtype/array-style-simple-type': ['error', 'verbose'],
 
-    "prettier/prettier": "error"
-  }
+    'react/jsx-indent': 'off',
+    'react/jsx-indent-props': 'off',
+    'react/jsx-wrap-multilinis': 'off',
+
+    'unicorn/filename-case': 'off',
+    'unicorn/catch-error-name': ['error', { name: 'err' }],
+  },
 };
 
 module.exports = config;
